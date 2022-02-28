21246fa097f61942c1d7081f9b03d8d9e35a73e7
AlvaroGutierrez96 <alvaro.gutierrez@bootcamp-upgrade.com>
2022-02-28 17:15:11 +0100

    Iteration#2

---
+++ ejercicioIteracióniteration#2.js
@@ -0,0 +1,16 @@
+const character = {name: 'Jack Sparrow', age: 10};
+age: 25;
+
+let firstName = "Jon"; 
+
+let lastName = "Snow"; 
+
+let age = 24; 
+
+
+console.log ('Soy' + firstName + ' ' + lastName + ', tengo ' + age + ' año y me gustan los lobos')
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price +  toy2.price)
